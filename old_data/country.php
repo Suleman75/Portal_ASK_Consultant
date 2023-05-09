b2807c1bf1d545a29f9af63c178451b3f58a9a99
Zia-ur-Rehman Khan <ziakhan1198@gmail.com>
2023-05-09 11:46:29 +0500

    DB Imported

---
+++ old_data/country.php
@@ -0,0 +1,29 @@
+<?php
+require("config.php");
+$file = fopen("inquiry1.csv","r");
+$i=0;
+$userData=array();
+while (($line = fgetcsv($file)) !== FALSE) {
+    //$line is an array of the csv elements
+    // print_r($line);
+    if($i==0)
+    {
+        $i++;
+        continue;
+    }
+    // $userData["id"]=$line[6];
+    array_push($userData,$line[6]);
+    // $user_id = insertData('user_info', $userData);
+    // echo $line[3]."<br>";
+  }
+  
+  $unique=array_unique($userData);
+  $data["country_name"]="";
+  var_dump($unique);
+  foreach($unique as $uni)
+  {
+    $data["country_name"]=$uni;
+    insertData('country', $data);
+  }
+fclose($file);
+?>
---
+++ old_data/final.php
@@ -0,0 +1,9 @@
+<?php
+require("config.php");
+$remove = array("@", "#", "(", ")", "*", "/","'",'"',"\\");
+require("test.php");
+require("test1.php");
+require("test2.php");
+require("test3.php");
+
+?>
