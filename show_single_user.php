218ab19339f5023e3b95f7933358b97ffceef303
Zia-ur-Rehman Khan <ziakhan1198@gmail.com>
2023-05-13 14:24:47 +0500

    added search button
    Remaining Stuff:
    login
    add users
    privilages
    allow adding priorities and delete them and stuff

--- show_single_user.php
+++ show_single_user.php
@@ -1,44 +1,16 @@
 <?php
 require("header.php");
 require("menu.php");
-if(!isset($_POST["min"]) || !isset($_POST["max"]))
-{
-    $_POST["min"]=1;
-    $_POST["max"]=1000;
-}
-if(isset($_POST["pagenumber"]))
-{
-    for($i=1;$i<$_POST["pagenumber"];$i++)
-    {
-        $_POST["min"]=$_POST["min"]+1000;
-        $_POST["max"]=$_POST["max"]+1000;
-    }
-    
-}
-$min=$_POST["min"];
-$max=$_POST["max"];
-$user_data=get_all_data($min,$max);
-
 ?>
 <form method="POST" action="show_single_user.php">
 <label>Search: </label><br><input type="text" name="user_id"><br><br>
 <input type="submit" value="Search"><br>
 </form>
 <?php
-
-$max_id=get_max_id("user_info");
-for($i=1;$i<=ceil($max_id/1000);$i++)
+if(isset($_POST["user_id"]))
 {
-    echo "<form method='POST' style='display:inline;'><input type='submit' name='pagenumber' value=$i></form>&nbsp;&nbsp;&nbsp;";
-}
-?>
-<br><br><br>
-<?php
-
-
-// $i=0;
-// $old_id=0;
-echo "<table>";
+    $user_data=get_single_user_data($_POST["user_id"]);
+    echo "<table>";
 echo "<tr>";
 echo "<th>S.No</th>";
 echo "<th>Lead priority</th>";
@@ -90,6 +62,17 @@ foreach($user_data as $rows)
     
 }
 echo "</table>";
+
+}
+
+
+?>
+
+<?php
+
+
+// $i=0;
+// $old_id=0;
 
 
 
